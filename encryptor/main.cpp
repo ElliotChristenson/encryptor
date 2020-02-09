14701b927325a602fffb3b445488d5f7e9c7ff2d
Elliot Christenson <elliot@greenbaynet.com>
2020-02-08 21:53:28 -0600

    Initial Commit

---
+++ encryptor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  encryptor
+//
+//  Created by Elliot Christenson on 2/8/20.
+//  Copyright © 2020 Elliot Christenson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
